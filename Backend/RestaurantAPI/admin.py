71bfab81ac5f024f40acc2ac81682180156ffc9e
lawminghan <lawminghan2000@gmail.com>
2023-02-25 23:34:16 +0800

    isolated some models into RestaurantAPI app

---
+++ Backend/RestaurantAPI/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from RestaurantAPI.models import Restaurant, Reservation, Review
+
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['resID', 'name', 'location', 'avg_rating', 'merchant']
+    
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display=['reservID', 'reserved_at', 'pax', 'restaurant', 'customer']
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['review_id', 'rating', 'description', 'restaurant', 'customer']
+    
---
+++ Backend/RestaurantAPI/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+class Restaurant(models.Model):
+    resID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    avg_rating = models.DecimalField(max_digits=3, decimal_places=2 ,default=0.0)
+    merchant = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        limit_choices_to={'groups__name': 'Merchants'},
+        related_name='restaurants' #user.restaurant will return the restaurants related to this Merchant
+        ) 
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class Reservation(models.Model):
+    reservID = models.AutoField(primary_key=True)
+    reserved_at = models.DateTimeField()
+    pax = models.IntegerField()
+    restaurant = models.ForeignKey(
+        Restaurant, 
+        on_delete=models.CASCADE,
+        related_name='reservations'
+        )
+    customer = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        limit_choices_to={'groups__name': 'Customers'},
+        related_name='reservations'
+        )
+    
+    def __str__(self):
+        return "Reservation {} by {} at {}".format(self.reservID, self.customer.get_username(), self.restaurant.name)
+    
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    rating = models.IntegerField(
+        choices=[(i, str(i)) for i in range(1, 6)],
+        default=0)
+    description = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(
+        Restaurant, 
+        on_delete=models.CASCADE,
+        related_name='reviews'
+        )
+    customer = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        limit_choices_to={'groups__name': 'Customers'},
+        related_name='reviews'
+        )
+    
+    def __str__(self):
+        return "Review {} by {} at {}".format(self.review_id, self.customer.get_username(), self.restaurant.name)
+    
