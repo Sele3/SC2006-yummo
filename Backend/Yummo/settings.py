34c081ef11e4e9b9e3e33874951fd7af95ff9bdc
lawminghan <lawminghan2000@gmail.com>
2023-02-25 23:39:38 +0800

    removed admin and models file from Yummo config folder

--- Backend/Yummo/settings.py
+++ Backend/Yummo/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Yummo',
+    'RestaurantAPI',
+    'YummoGroupAPI',
+
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kuala_Lumpur' #UTC+8
 
 USE_I18N = True
 
