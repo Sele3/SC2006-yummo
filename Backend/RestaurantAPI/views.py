bb2dbcb69d914584c37e7167439cd5edd5f36b36
lawminghan <lawminghan2000@gmail.com>
2023-02-27 02:11:34 +0800

    created utilityfunctions file

--- Backend/RestaurantAPI/views.py
+++ Backend/RestaurantAPI/views.py
@@ -9,20 +9,20 @@ from .models import Restaurant, Reservation, Review
 from .serializers import RestaurantSerializer, ReservationSerializer, ReviewSerializer
 from django.core.paginator import Paginator, EmptyPage
 import datetime
+from Yummo.utilityfunctions import isCustomerGroup, isMerchantGroup
 # Create your views here.
-
-
 
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def restaurantsView(request):
+    if not isMerchantGroup(request):
+        return Response({'message':"Only Merchants access this endpoint"},status.HTTP_403_FORBIDDEN)    
     if request.method == 'GET':
-       #restaurant_list = Restaurant.objects.filter(merchant=request.user)
         restaurant_list = request.user.restaurants
         serialized_restaurant_list = RestaurantSerializer(restaurant_list, many=True)
         return Response(serialized_restaurant_list.data)
-    
+
     if request.method == 'POST':
         data = request.data.copy() #make it mutable
         data['merchant'] = request.user.id #current User is the Merchant for the Restaurant
@@ -33,9 +33,35 @@ def restaurantsView(request):
     
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def singleRestaurantView(request, id):
-    pass
+    #Customers, Merchants
+    if request.method == 'GET': 
+        restaurant = get_object_or_404(Restaurant, pk=id) 
+        serialized_restaurant = RestaurantSerializer(restaurant)
+        return Response(serialized_restaurant.data)
+    
+    #Merchants only
+    if not isMerchantGroup(request):
+        return Response({'message':"Only Merchants access this endpoint"},status.HTTP_403_FORBIDDEN) 
+    
+    if request.method == 'PUT' or 'PATCH':
+        restaurant = get_object_or_404(Restaurant, pk=id, merchant=request.user)
+        data = request.data.copy()
+        data['merchant'] = request.user.id
+        partial = True
+        if request.method == 'PUT':
+            partial = False
+        serialized_restaurant = RestaurantSerializer(restaurant,data=data, partial=partial)
+        serialized_restaurant.is_valid(raise_exception=True)
+        serialized_restaurant.save()
+        return Response(serialized_restaurant.data, status=status.HTTP_200_OK)
+        
+    if request.method == 'DELETE':
+        restaurant = get_object_or_404(Restaurant, pk=id, merchant=request.user) #find specified Restaurant owned by this Merchant
+        restaurant.delete()
+        return Response({"message":"Restaurant deleted"}, status=status.HTTP_200_OK)
+    
 
 
 
--- Backend/YummoGroupAPI/views.py
+++ Backend/YummoGroupAPI/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from Yummo.utilityfunctions import isCustomerGroup, isMerchantGroup
 # Create your views here.
