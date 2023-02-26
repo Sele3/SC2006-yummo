3f3acc1cba6ab1c8a50d888e85404f2ed49a6131
xotong <xotong.xo@gmail.com>
2023-02-26 11:56:48 +0800

    FrontEnd ReactApp Built
    - Added Full React Installation
    - Added Axios for API Handling
    - Implemented Testing for Axios

---
+++ Frontend/yummo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from './components/PersonList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Persons</h3>
+      <PersonList />
+    </div>
+  );
+}
+
+export default App;
