11ec5cd15b9caab098307713e5a84218ef362b46
xotong <xotong.xo@gmail.com>
2023-03-09 20:26:11 +0800

    Update Fonts for letsyummolocation

--- Frontend/yummo/src/components/page/_41letsyummolocation.js
+++ Frontend/yummo/src/components/page/_41letsyummolocation.js
@@ -3,17 +3,9 @@ import './_41letsyummolocation.css'
 import GoogleLocation from './googlelocation.js'
 
 export default function _41letsyummolocation () {
-	const [data, setData] = useState({
-		inputValue: '?',
-	})
-
-	const sendData = (data) => {
-		setData(data)
-	}
-
 	return (
 		<>
-		<div className='h1'>
+		<div>
 			<section>
 				<yummo>
 					<span className='front'>Yum</span>
