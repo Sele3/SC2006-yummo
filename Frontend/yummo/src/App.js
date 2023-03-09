9a1557150dc76e26ddfa922e8e60753a6c72d234
Sele3 <STAN266@e.ntu.edu.sg>
2023-03-09 21:37:49 +0800

    Revert "Merge branch 'frontend-ivan'"
    This reverts commit 45ad8a6443628372f810b21d1065b6b55bbde4bd, reversing
    changes made to be5563ba79a9af99ab5745ba80fba4053848a341.

--- Frontend/yummo/src/App.js
+++ Frontend/yummo/src/App.js
@@ -1,9 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
-import Routing from './components/Routing.js';
+import PersonList from './components/PersonList.js';
 
 function App() {
   return (
-    <Routing />
+    <div className="App">
+      <h3>Persons</h3>
+      <PersonList />
+    </div>
   );
 }
 
