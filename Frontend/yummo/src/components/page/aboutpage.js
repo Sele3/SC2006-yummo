97a44e390db7fda19004c9700589acb5471e5e2a
xotong <xotong.xo@gmail.com>
2023-03-09 19:40:39 +0800

    Added LetsYummoLocation
    Added MUI Autocomplete GoogleMaps API Box
    User inputted location will be passed to the /aboutpage, where data is POST-ed to Backend API.

---
+++ Frontend/yummo/src/components/page/aboutpage.js
@@ -0,0 +1,46 @@
+import { useLocation, Link } from "react-router-dom";
+import { useState } from "react";
+import axios from 'axios';
+
+const AboutPage = (props) => {
+  const location = useLocation();
+  const state = location.state;
+  console.log(state);
+
+  const url = 'http://127.0.0.1:8000/api/restaurants/recommendations';
+  const token = '1b4a3a59041c56c203a4f44e77f831bd705c029f';
+  const data = {state};
+
+  const [result, setResult] = useState({});
+
+  axios.post(url, data, {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Token ${token}`,
+    },
+  })
+    .then(res => {
+        setResult(res.data);
+        console.log(result);
+    })
+    .catch(err => {
+        console.error(err);
+    })
+
+  return (
+    <>
+      <h1>This is About page</h1>
+      {state && (
+        <div>
+          <h3>Passed data:</h3>
+          <p>InputLocation: {state}</p>
+          <p>API Data: {}</p>
+        </div>
+      )}
+      <hr />
+      <Link to="/">Go Home</Link>
+    </>
+  );
+};
+
+export default AboutPage;
