63ca9017ce24f4ec4a95398c7f1b90d281cb1176
Sele3 <STAN266@e.ntu.edu.sg>
2023-03-10 02:40:45 +0800

    refactor: standardize response title
    standardize all response title to `message`

--- Backend/YummoGroupAPI/views.py
+++ Backend/YummoGroupAPI/views.py
@@ -76,14 +76,14 @@ class SingleFriendView(AuthenticatedCustomerViewClass):
             return Response({"message": str(e)}, status=e.get_status_code()) 
         
         if current_user_profile.user.username == username:
-            return Response({'error': 'Unable to add yourself as friend.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Unable to add yourself as friend.'}, status=status.HTTP_400_BAD_REQUEST)
         
         friend_user = self.get_requested_friend(username)
         if friend_user in current_user_profile.friends.all():
-            return Response({'error': f'{username} is already in friends list.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': f'{username} is already in friends list.'}, status=status.HTTP_400_BAD_REQUEST)
 
         current_user_profile.friends.add(friend_user)
-        return Response({'success': f'{username} has been added to your friends list.'}, status=status.HTTP_201_CREATED)
+        return Response({'message': f'{username} has been added to your friends list.'}, status=status.HTTP_201_CREATED)
 
     @swagger_auto_schema(tags=['friends'], responses={200: "OK", 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
     def delete(self, request, username):
@@ -93,14 +93,14 @@ class SingleFriendView(AuthenticatedCustomerViewClass):
             return Response({"message": str(e)}, status=e.get_status_code()) 
 
         if current_user_profile.user.username == username:
-            return Response({'error': 'Unable to remove yourself as friend.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Unable to remove yourself as friend.'}, status=status.HTTP_400_BAD_REQUEST)
 
         friend_user = self.get_requested_friend(username)
         if friend_user not in current_user_profile.friends.all():
-            return Response({'error': f'{username} is not in your friends list.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': f'{username} is not in your friends list.'}, status=status.HTTP_400_BAD_REQUEST)
 
         current_user_profile.friends.remove(friend_user)
-        return Response({'success': f'{username} has been removed from your friends list.'}, status=status.HTTP_200_OK)
+        return Response({'message': f'{username} has been removed from your friends list.'}, status=status.HTTP_200_OK)
 
 
 class YummoGroupsView(AuthenticatedCustomerViewClass):
