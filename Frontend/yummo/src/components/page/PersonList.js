84a9b38b92b5d1980d4cfd0ba649a94c506c7cd6
Sele3 <STAN266@e.ntu.edu.sg>
2023-03-11 01:18:34 +0800

    fix: reset file tracking

---
+++ Frontend/yummo/src/components/page/PersonList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import './PersonList.css';
+import { useLocation } from 'react-router-dom';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+        <><div className="PersonList">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                {this.state.persons.map(person => {
+                    return (
+                        <tr key={person.id}>
+                            <td>{person.name}</td>
+                            <td>{person.phone}</td>
+                            <td>{person.email}</td>
+                        </tr>
+                    );
+                })}
+            </table>
+            <p>Location: {this.inputValue}</p>
+        </div>
+        </>
+    )
+  }
+}
