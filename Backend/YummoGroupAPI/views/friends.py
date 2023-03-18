40ad84fc078b73e5bfe0347eb66ce33be7d5661d
Sele3 <STAN266@e.ntu.edu.sg>
2023-03-19 00:20:08 +0800

    docs: add operation description to all endpoints

--- Backend/YummoGroupAPI/views/friends.py
+++ Backend/YummoGroupAPI/views/friends.py
@@ -10,7 +10,10 @@ from drf_yasg.utils import swagger_auto_schema
 
 
 class FriendsView(AuthenticatedCustomerViewClass):
-    @swagger_auto_schema(tags=['friends'], responses={200: UserBasicInfoSerializer(many=True)})
+    @swagger_auto_schema(
+        operation_description="Get the list of `Friends` of the current Customer.",
+        tags=['friends'], 
+        responses={200: UserBasicInfoSerializer(many=True)})
     def get(self, request):
         profile = get_object_or_404(CustomerProfile, user=request.user)
         serialized_profile = UserBasicInfoSerializer(profile.friends, many=True)
@@ -22,19 +25,19 @@ class SingleFriendView(AuthenticatedCustomerViewClass):
         return get_object_or_404(User, username=username)
 
 
-    @swagger_auto_schema(tags=['friends'], responses={200: UserBasicInfoSerializer, 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
+    @swagger_auto_schema(
+        operation_description="Get basic information of a `Customer` profile.",
+        tags=['friends'], 
+        responses={200: UserBasicInfoSerializer, 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
     def get(self, request, username):
-        current_user_profile = getProfile(request)
         friend_user = self.get_requested_friend(username)
-
-        if friend_user not in current_user_profile.friends.all():
-            return Response({"message": "You are not friends with this user"}, status=status.HTTP_403_FORBIDDEN)
-        
-        friend_profile = get_object_or_404(User, id=friend_user.id)
-        serialized_friend = UserBasicInfoSerializer(friend_profile)
+        serialized_friend = UserBasicInfoSerializer(friend_user)
         return Response(serialized_friend.data, status=status.HTTP_200_OK)
         
-    @swagger_auto_schema(tags=['friends'], responses={201: "Created", 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
+    @swagger_auto_schema(
+        operation_description="Add a new `Customer` to current Customer's friend list.",
+        tags=['friends'], 
+        responses={201: "Created", 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
     def post(self, request, username):
         current_user_profile = getProfile(request)
         
@@ -48,7 +51,10 @@ class SingleFriendView(AuthenticatedCustomerViewClass):
         current_user_profile.friends.add(friend_user)
         return Response({'message': f'{username} has been added to your friends list.'}, status=status.HTTP_201_CREATED)
 
-    @swagger_auto_schema(tags=['friends'], responses={200: "OK", 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
+    @swagger_auto_schema(
+        operation_description="Remove a `Customer` from current Customer's friend list.",
+        tags=['friends'], 
+        responses={200: "OK", 400: "Bad Request", 403: "Forbidden", 404: "Not Found"})
     def delete(self, request, username):
         current_user_profile = getProfile(request)
 
