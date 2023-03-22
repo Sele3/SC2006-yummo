e8049c53f93f2f2d137c55367f4d5502a2273b50
xotong <xotong.xo@gmail.com>
2023-03-22 18:03:28 +0800

    YummoReservation Working V2

--- Frontend/yummo/src/components/page/yummoreservation.js
+++ Frontend/yummo/src/components/page/yummoreservation.js
@@ -7,6 +7,7 @@ import MoneyOffIcon from '@mui/icons-material/MoneyOff';
 import Autocomplete from '@mui/material/Autocomplete';
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import DatePicker from "react-datepicker";
 import NavBar from "../navbar.js";
 import MapContainer from "../map.js";
@@ -66,6 +67,7 @@ const NumPax = (props) => {
                     boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
                     cursor: 'pointer'
                 }}
+                onClick={() => props.handleNext(1)}
             >
             Next
             </button>
@@ -75,14 +77,20 @@ const NumPax = (props) => {
     );
   };
   
-const TimeSelection = () => {
+const TimeSelection = (props) => {
     const [startDate, setStartDate] = useState(new Date());
-    /*const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
       <button className="example-custom-input" onClick={onClick} ref={ref}>
         {value}
       </button>
-    ));*/
+    ));
     const [time, setTime] = useState("12:00");
+
+    useEffect(() => {
+        props.handleTime(time);
+        props.handleDate(startDate);
+    }, [time, startDate]);
+
     const timing = [
         "10:00",
         "10:30",
@@ -112,27 +120,29 @@ const TimeSelection = () => {
     return (
         <>
         <div className="date-selection-container">
+            <h1>Confirm your selection</h1>
             <div className="date-container">
                 <div className="date-title">
-                    <h1>Date:</h1>
+                    <h2>Date:</h2>
                 </div>
                 <div className="date-content">
                     <DatePicker
                     selected={startDate}
                     onChange={(date) => setStartDate(date)}
+                    customInput={<ExampleCustomInput />}
                     />
                 </div>
             </div>
             <div className="time-container">
                 <div className="time-title">
-                    <h1>Time:</h1>
+                    <h2>Time:</h2>
                 </div>
                 <div className="time-content">
                     <Autocomplete
                     disablePortal
                     id="combo-box-demo"
                     options={timing}
-                    sx={{ width: '10rem' }}
+                    sx={{ width: '10rem', backgroundColor: '#FFD600', fontWeight: 'bold' }}
                     onInputChange={(event, newTime) => {
                         setTime(newTime);
                     }}
@@ -140,8 +150,30 @@ const TimeSelection = () => {
                     />
                 </div>
             </div>
-        </div>
-        <div className="confirm-button-container">
+            <div className="confirm-button-container">
+                <button 
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        position: 'relative',
+                        fontSize: '1rem',
+                        padding: '12px 24px',
+                        backgroundColor: '#000000',
+                        color: '#FFD600',
+                        borderRadius: '2rem',
+                        fontFamily: 'Roboto',
+                        fontWeight: 'bold',
+                        textTransform: 'uppercase',
+                        letterSpacing: '0.2rem',
+                        border: 'none',
+                        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+                        cursor: 'pointer'
+                    }}
+                >
+                Confirm
+                </button>
+            </div>
         </div>
         </>
     );
@@ -167,9 +199,25 @@ export default function Yummoreservation(props) {
         setSelectedName(state.selectedName);
     }, [state]);
 
+    const [nextClicked, setNextClicked] = useState(false);
+
     const [pax, setPax] = useState(0);
+    const [date, setDate] = useState(new Date());
+    const [time, setTime] = useState("");
     function handlePax(value) {
         setPax(value);
+    }
+    function handleDate(value) {
+        setDate(value);
+        console.log("new date: ", value);
+    }
+    function handleTime(value) {
+        setTime(value);
+        console.log("new time: ", value);
+    }
+    function handleNext() {
+        setNextClicked(true);
+        console.log("next clicked");
     }
 
     return (
@@ -200,8 +248,16 @@ export default function Yummoreservation(props) {
                     </div>
             </div>
             <div className="pax-time-container">
-                {/*<NumPax numPax={pax} handlePax={handlePax}/>*/}
-                <TimeSelection />
+                <TransitionGroup>
+                    <CSSTransition
+                        in={nextClicked}
+                        classNames="slide"
+                        timeout={800}
+                        unmountOnExit
+                    >
+                        {nextClicked ? <TimeSelection handleDate={handleDate} handleTime={handleTime}/> : <NumPax numPax={pax} handlePax={handlePax} handleNext={handleNext}/>}
+                    </CSSTransition>
+                </TransitionGroup>
             </div>
         </div>
         </>
