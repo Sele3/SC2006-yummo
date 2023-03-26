d5daafd0ef79fba38438f4464c4f7f36d0cbe91e
Ming Han <101275733+lawminghan@users.noreply.github.com>
2023-03-26 19:57:10 +0800

    feat: create CustomerReservations endpoint
    - Returns a list of all the reservation sorted by latest datetime, made by a customer across all yummo restaurants

--- Backend/RestaurantAPI/urls.py
+++ Backend/RestaurantAPI/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
    path('restaurants/<int:resID>', restaurants.SingleRestaurantView.as_view()),
    path('restaurants/recommendations', recommendations.RestaurantRecommendationsView.as_view()),
    path('restaurants/search', recommendations.SearchRestaurantsView.as_view(), name='searchRestaurants'),
+   path('restaurants/reservations', reservations.CustomerReservationsView.as_view()),
    path('restaurants/<int:resID>/reservations', reservations.ReservationsView.as_view()),
    path('restaurants/<int:resID>/reservations/<int:reservID>', reservations.SingleReservationView.as_view()),
    path('restaurants/<int:resID>/reviews', reviews.ReviewsView.as_view()),
