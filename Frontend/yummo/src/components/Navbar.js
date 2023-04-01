78360a7503f3ee1789e41ee9e895b4ba07d2b9d3
Sele3 <STAN266@e.ntu.edu.sg>
2023-04-02 00:41:09 +0800

    feat: merchant overview page

--- Frontend/yummo/src/components/Navbar.js
+++ Frontend/yummo/src/components/Navbar.js
@@ -1,13 +1,12 @@
-import { NavLink, Link } from "react-router-dom";
+import axios from "axios";
+import { Link, NavLink } from "react-router-dom";
+import { COLOR_DEFAULT, COLOR_MERCHANT } from "../constants";
 import LogoDefault from "./LogoDefault";
 import LogoMerchant from "./LogoMerchant";
 import styles from "./Navbar.module.css";
-import axios from "axios";
+import ChangeRestaurantButton from "./page/merchant/ChangeRestaurantButton";
 
-const COLOR_DEFAULT = "#ffdd40";
-const COLOR_MERCHANT = "#00aeb2";
-
-const ROUTES = {
+const routes = {
   customer: [
     {
       name: "FoodRecco",
@@ -15,7 +14,7 @@ const ROUTES = {
     },
     {
       name: "Feed",
-      path: "/about",
+      path: "/feed",
     },
     {
       name: "My Reservations",
@@ -28,14 +27,6 @@ const ROUTES = {
       path: "/merchant",
     },
     {
-      name: "Reservations",
-      path: "/merchant/reservations",
-    },
-    {
-      name: "Reviews",
-      path: "/merchant/reviews",
-    },
-    {
       name: "Account",
       path: "/merchant/account",
     },
@@ -45,16 +36,20 @@ const ROUTES = {
 function NavBar({ isMerchant = false }) {
   const color = isMerchant ? COLOR_MERCHANT : COLOR_DEFAULT;
   const Logo = isMerchant ? LogoMerchant : LogoDefault;
-  const navRoutes = isMerchant ? ROUTES.merchant : ROUTES.customer;
+  const navRoutes = isMerchant ? routes.merchant : routes.customer;
 
   const handleLogoutClick = () => {
     const url = "http://127.0.0.1:8000/auth/token/logout/";
     const token = localStorage.getItem("authToken");
-    axios.post(url, {}, {
+    axios.post(
+      url,
+      {},
+      {
         headers: {
-            Authorization: `Token ${token}`,
-            },
-    })
+          Authorization: `Token ${token}`,
+        },
+      }
+    );
   };
 
   return (
@@ -68,45 +63,52 @@ function NavBar({ isMerchant = false }) {
         <Logo />
       </NavLink>
 
-      <ul className={styles.container}>
-        {navRoutes.map(({ name, path }) => (
-          <li key={name}>
-            <NavLink
-              to={path}
-              end
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.item
-              }
-            >
-              {name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-      <Link to="/">
-            <button
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        position: 'relative',
-                        fontSize: '1rem',
-                        padding: '12px 24px',
-                        backgroundColor: '#000000',
-                        color: '#FFD600',
-                        borderRadius: '2rem',
-                        fontFamily: 'Roboto',
-                        fontWeight: 'bold',
-                        textTransform: 'uppercase',
-                        letterSpacing: '0.2rem',
-                        border: 'none',
-                        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
-                        cursor: 'pointer',
-                        justifyContent: 'center',
-                        marginLeft: '2rem',
-                    }}
-                    onClick={handleLogoutClick}
-            >Logout</button>
-    </Link>
+      <div className={styles.container}>
+        <ul className={styles.navLinks}>
+          {navRoutes.map(({ name, path }) => (
+            <li key={name}>
+              <NavLink
+                to={path}
+                end
+                className={({ isActive }) =>
+                  isActive ? styles.active : styles.item
+                }
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        {isMerchant && <ChangeRestaurantButton />}
+
+        <Link to="/">
+          <button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              position: "relative",
+              fontSize: "1rem",
+              padding: "12px 24px",
+              backgroundColor: "#000000",
+              color: "#FFD600",
+              borderRadius: "2rem",
+              fontFamily: "Roboto",
+              fontWeight: "bold",
+              textTransform: "uppercase",
+              letterSpacing: "0.2rem",
+              border: "none",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+              cursor: "pointer",
+              justifyContent: "center",
+              marginLeft: "2rem",
+            }}
+            onClick={handleLogoutClick}
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
     </nav>
   );
 }
--- Frontend/yummo/src/components/Routing.js
+++ Frontend/yummo/src/components/Routing.js
@@ -4,11 +4,13 @@ import LetsYummoCraving from "./page/letsyummocraving.js";
 import YummoSuggestions from "./page/yummosuggestions.js";
 import YummoReservation from "./page/yummoreservation.js";
 import YummoConfirmation from "./page/yummoconfirmation.js";
-import MerchantOverview from "./page/merchantpageoverview";
+import MerchantDashboard from "./page/merchant/MerchantDashboard.js";
 import Login from "./page/Login";
 import { Routes, Route } from "react-router-dom";
 import Customer_Acc_Setting from "./page/Customer_Acc_Sett.js";
 import MerchantAddRestaurant from "./page/merchantAddRestaurant.js";
+//import MerchantAddRestaurant from "./page/merchantAddRestaurant.js";
+import RestaurantOverview from "./page/merchant/RestaurantOverview.js";
 import MerchantPageAccount from "./page/merchantpageaccount.js";
 import MerchantLogin from "./page/merchantLogin.js";
 import MerchantRegister from "./page/merchantRegister.js";
@@ -54,18 +56,10 @@ function Routing() {
         />
       </Route>
 
-      <Route
-        path="/merchant"
-        element={
-          <>
-            <NavBar isMerchant />
-            <Outlet />
-          </>
-        }
-      >
+      <Route path="/merchant" element={<MerchantDashboard />}>
+        <Route index element={<RestaurantOverview />} />
       </Route>
 
-      <Route path="/merchantoverview" element={<MerchantOverview />} />
       <Route path="/merchantPageAccount" element={<MerchantPageAccount />} />
       <Route path="/merchantLogin" element={<MerchantLogin />} />
       <Route path="/merchantRegister" element={<MerchantRegister />} />
